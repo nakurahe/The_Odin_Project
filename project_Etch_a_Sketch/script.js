041fa678162ed248846f4d06faca2b47d59f2267
Nakura <126201955+nakurahe@users.noreply.github.com>
2024-10-11 17:58:17 -0700

    Finished extra credit1 with bug\n
    Now when in RGB ON mode if the user clicked change size then RGB mode will not be recorded.

--- project_Etch_a_Sketch/script.js
+++ project_Etch_a_Sketch/script.js
@@ -1,19 +1,48 @@
 const selectionArea = document.querySelector('.selection-area');
 const colorBtn = document.querySelector('#color');
-colorBtn.testContent = 'RGB ON';
+colorBtn.addEventListener('click', () => {
+    // Change the text content of the button.
+    colorBtn.textContent = colorBtn.textContent === 'RGB ON' ? 'RGB OFF' : 'RGB ON';
+
+    if (colorBtn.textContent === 'RGB ON') {
+        // Set the color of all cells to black.
+        for (let row of sketchArea.children) {
+            for (let cell of row.children) {
+                setCellStyle(cell);
+            }
+        }
+    } else if (colorBtn.textContent === 'RGB OFF') {
+        // Set the color of all cells to random RGB values.
+        for (let row of sketchArea.children) {
+            for (let cell of row.children) {
+                // Randomly generate a color for each cell, which is ugly.
+                setCellStyle(cell, `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`);
+            }
+        }
+    }
+});
+
 
 const resizeBtn = document.querySelector('#resize');
-resizeBtn.textContent = 'Resize';
-resetBtn.addEventListener('click', () => {
-    
-    const cells = document.querySelectorAll('.sketch-area div div');
-    cells.forEach(cell => {
-        cell.style.backgroundColor = 'white';
-    });
+resizeBtn.addEventListener('click', () => {
+    let input = prompt('Enter a number between 1 and 100 (inclusive).');
+    input = parseInt(input);
+    if (input < 1 || input > 100 || isNaN(input)) {
+        alert('Invalid input. Please enter a number between 1 and 100 (inclusive).');
+    } else {
+        sketchArea.innerHTML = '';
+        createGrid(input);
+    }
 });
 
 const resetBtn = document.querySelector('#reset');
-resetBtn.textContent = 'Reset';
+resetBtn.addEventListener('click', () => {
+    for (let row of sketchArea.children) {
+        for (let cell of row.children) {
+            cell.style.backgroundColor = 'transparent';
+        }
+    }
+});
 
 const ROWS = 16;
 const COLS = 16;
@@ -28,26 +57,30 @@ function setRowStyle(row) {
     row.style.height = '100%';
     row.style.width = '100%';
     // I'll keep the gap here for reference. Will remove it later.
-    row.style.gap = '3px';
+    // row.style.gap = '3px';
 }
 
 // Function to set cells in a row to a specific style.
-function setCellStyle(cell) {
+function setCellStyle(cell, color='black') {
     cell.style.height = '100%';
     cell.style.width = '100%';
     cell.addEventListener('mouseover', () => {
-        cell.style.backgroundColor = 'black';
+        cell.style.backgroundColor = color;
     });
 }
 
 // Create 16x16 grid of square divs and append them to sketch area.
-for (let row = 0; row < ROWS; row++) {
-    const rowDiv = document.createElement('div');
-    setRowStyle(rowDiv);
-    for (let col = 0; col < COLS; col++) {
-        const cell = document.createElement('div');
-        setCellStyle(cell);        
-        rowDiv.appendChild(cell);
+function createGrid(rows=ROWS, cols=COLS) {
+    for (let row = 0; row < rows; row++) {
+        const rowDiv = document.createElement('div');
+        setRowStyle(rowDiv);
+        for (let col = 0; col < cols; col++) {
+            const cell = document.createElement('div');
+            setCellStyle(cell);
+            rowDiv.appendChild(cell);
+        }
+        sketchArea.appendChild(rowDiv);
     }
-    sketchArea.appendChild(rowDiv);
 }
+
+createGrid();
