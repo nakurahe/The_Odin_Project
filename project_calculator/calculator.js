e40ccc96ec66bd64fe2fdcb3946396d844b2d45e
Nakura <126201955+nakurahe@users.noreply.github.com>
2024-10-16 18:31:16 -0700

    Update basic oepration calculation with float, percentage calculation, and TODOs

--- project_calculator/calculator.js
+++ project_calculator/calculator.js
@@ -1,21 +1,24 @@
 // Functions to perform basic arithmetic operations.
 function add(number1, number2) {
-    return number1 + number2;
+    return parseFloat((number1 + number2).toFixed(9));
 }
 
 function subtract(number1, number2) {
-    return number1 - number2;
+    return parseFloat((number1 - number2).toFixed(9));
 }
 
 function multiply(number1, number2) {
-    return number1 * number2;
+    return parseFloat((number1 * number2).toFixed(9));
 }
 
 function divide(number1, number2) {
     if (number2 === 0) {
         return "Error: Division by zero";
     }
-    return number1 / number2;
+    return parseFloat((number1 / number2).toFixed(9));
+}
+function percentage(number) {
+    return parseFloat((number / 100).toFixed(9));
 }
 
 function operate(operator, number1, number2) {
@@ -55,8 +58,18 @@ function appendChar(char) {
 // Function to calculate the result based on the current expression.
 function calculate() {
     const expression = display.textContent;
-    const regex = /(\d+)([+\-*/])(\d+)/;
-    const match = expression.match(regex);
+    let regex = /(\d*.*\d+)([+\-*/])(\d*.*\d+)/;
+    let match = expression.match(regex);
+    if (match) {
+        const number1 = parseFloat(match[1]);
+        const operator = match[2];
+        const number2 = parseFloat(match[3]);
+        const result = operate(operator, number1, number2);
+        return result;
+    }
+    
+    regex = /(\d+)([+\-*/])(\d+)/;
+    match = expression.match(regex);
     if (match) {
         const number1 = parseInt(match[1]);
         const operator = match[2];
@@ -64,6 +77,13 @@ function calculate() {
         const result = operate(operator, number1, number2);
         return result;
     }
+
+    if (expression.includes("%")) {
+        const number = parseFloat(expression);
+        const result = percentage(number);
+        return result;
+    }
+
     return display.textContent;
 }
 
@@ -90,6 +110,11 @@ basicOperatorButtons.forEach(button => button.addEventListener("click", () => {
     const currentDisplay = display.textContent;
     if (/[+\-*/]/.test(currentDisplay)) {
         const result = calculate();
-        display.textContent = result ;
+        display.textContent = result;
     } 
 }));
+
+// TODO: Add keyboard support.
+// TODO: Erase numbers in display after one calculation.
+// TODO: Pressing = before entering all of the numbers or an operator could cause problems!
+// TODO: Other extra credit features.
